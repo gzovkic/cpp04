e6af719d679df977a2c0d399f2b981245eb29ff5
gzovkic <g.zovkic@web.de>
2025-12-30 16:34:48 +0100

    added folder and basic structure for ex03

---
+++ ex03/inc/AMateria.hpp
@@ -0,0 +1,12 @@
+#include "Colors.hpp"
+
+class AMateria
+{
+    protected:
+    
+    public:
+        AMateria(std::string const & type);
+        std::string const & getType() const;
+        virtual AMateria* clone() const = 0;
+        // virtual void use(ICharacter& target);
+};
